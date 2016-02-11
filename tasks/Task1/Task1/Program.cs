d6167e23ff322c4cc09091f8b22263219e28943d
David <dav.gayer@gmail.com>
2016-02-11 19:57:00 +0100

    add projekt Task1

---
+++ tasks/Task1/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var i = "Hello World";
+            var k = "!";
+            Console.BackgroundColor= ConsoleColor.Magenta;
+            Console.WriteLine($"{i}{k}");
+
+        }
+    }
+}
