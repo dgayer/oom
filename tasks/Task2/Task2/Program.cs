2652e61a61b4e3b5282dee0f10d4c98b1f3f9746
David <dav.gayer@gmail.com>
2016-02-22 18:42:18 +0100

    Homework Task2 added

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w1 = new Weapon("M-16", 7.25m, 30);
+            Weapon w2 = new Weapon(25, 7.25m);
+            Weapon w3 = new Weapon("AK-47", 6.40m);
+
+            Console.WriteLine($"Weapon 1 Caliber: {w1.Caliber}");
+            Console.WriteLine($"Weapon 2 Name: {w2.Name}");
+            w2.Name = "Five-Seven";
+            Console.WriteLine($"Weapon 2 Name: {w2.Name}");
+            Console.WriteLine($"Clipsize in Weapon 2 upated to {w2.UpdateClipsize(30)}");
+
+        }
+    }
+}
---
+++ tasks/Task2/Task2/weapon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Weapon
+    {
+
+        private int clipSize;
+      
+        
+
+        public Weapon(int clipSize, decimal caliber)
+        {
+            this.Name = string.Empty;
+            this.Caliber = caliber;
+            this.clipSize = clipSize;
+        }
+
+        public Weapon(string name, decimal caliber)
+        {
+            this.Name = name;
+            this.clipSize = 30;
+            this.Caliber = caliber;
+        }
+
+        public Weapon(string name, decimal caliber, int clipSize)
+        {
+            this.Name = name;
+            this.clipSize = clipSize;
+            this.Caliber = caliber;
+        }
+
+       
+
+        public string Name { get; set; }
+        public decimal Caliber { get; private set; }
+
+
+        public int UpdateClipsize(int newClipSize)
+        {
+            if (clipSize <= 0)
+            {
+                Console.WriteLine("new clipsize must not be 0!");
+                return clipSize;
+            }
+            else
+            {
+                clipSize = newClipSize;
+                return newClipSize;
+            }
+        }
+    }
+}
